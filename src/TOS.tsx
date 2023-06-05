78c1e3dd2f43ec9999f3226eeba82d6beec3c55d
th-m <thomvaladez@gmail.com>
2023-06-05 16:53:33 -0600

    tos and pp

---
+++ src/TOS.tsx
@@ -0,0 +1,248 @@
+export function TOS() {
+  return (
+    <>
+      <p>
+        You and Harmony Hub shall also hereinafter be referred to together as
+        the “Parties” and individually as a “Party”.
+      </p>
+      <p>
+        Please read the terms and conditions of this Agreement carefully before
+        using the Website and Services. Your access to the Website and your use
+        of the Services (as defined below) is conditioned upon your acceptance
+        of and compliance with this Agreement.
+      </p>
+      <h2> ACCEPTANCE AND MODIFICATION OF THIS AGREEMENT</h2>
+      <p>
+        {" "}
+        By accessing the Website and using the Services (defined below) you
+        agree to be bound by this Agreement and accept to comply with all
+        applicable laws and regulations. Should you disagree to be bound by this
+        Agreement, in whole or in part, you shall (i) not be
+        permitted/authorized to use the Services (as defined below), and (ii)
+        refrain from accessing the Website.
+      </p>
+      <p>
+        {" "}
+        Harmony Hub may, in its sole discretion, modify or replace the terms and
+        conditions of this Agreement from time to time. Your continued
+        use/access to the Website and Services (as defined below) following the
+        posting of any changes to this Agreement shall be deemed to constitute
+        your acceptance of this Agreement. &nbsp
+      </p>
+      <h2>THE SERVICES</h2>
+      <p>
+        {" "}
+        Harmony Hub provides a centralized hub for developer centric
+        productivity tracking apps:
+      </p>
+      <h2>ACCEPTABLE USE OF THE WEBSITE</h2>
+      You may not use the Website to transmit, distribute, store or destroy any
+      material or information (i) in violation of any applicable law or
+      regulation; (ii) in a manner that infringes Harmony Hub’s Intellectual
+      Property Rights (defined below) or any third-party’s intellectual property
+      rights; (iii) in a manner that violates the privacy or other personal
+      rights of third parties; (iv) that is defamatory, damaging, disruptive,
+      obscene, threatening, abusive or hateful, and/or (v) in order to upload,
+      post, email, transmit, or otherwise make available any content that shall
+      (a) be deemed unlawful, harmful, threatening, abusive, harassing,
+      tortious, vulgar, obscene, libelous, or invasive of another's privacy or
+      which is hateful, and/or racially, ethnically, or otherwise objectionable;
+      and (b) incite, encourage or threaten immediate physical harm against
+      another, promotes racism, bigotry, sexism, religious intolerance or harm
+      against any group or individual; contains material that solicits personal
+      information from anyone under the age of eighteen (18) or exploits anyone
+      in a sexual or violent manner.
+      <h2> DISCLAIMER OF WARRANTIES</h2>
+      <p>
+        {" "}
+        Unless otherwise provided under this Agreement, the Services shall be
+        provided by Harmony Hub to you "as is," and “as available” with all
+        faults, defects, bugs, and errors.
+      </p>
+      <p>
+        {" "}
+        Harmony Hub hereby (i) disclaims all warranties and conditions with
+        regard to your use of the services on the website; and (ii) makes no
+        warranties or guarantees of any kind, including all warranties and
+        conditions of merchantability, whether express, implied or statutory,
+        fitness for a particular purpose, title, non-infringement, quiet
+        enjoyment or integration.
+      </p>
+      <p>
+        {" "}
+        Harmony Hub and/or its respective affiliates (if any), licensors and
+        vendors make no representations about the suitability, accuracy,
+        reliability or completeness of (i) the information contained on the
+        website and services, and (ii) related graphics published on the website
+        and services for any purpose. Harmony Hub and/or its respective
+        affiliates (if any) hereby disclaim all warranties and conditions with
+        regard to the information contained on the website and services,
+        including all warranties and conditions of merchantability, whether
+        express, implied or statutory, fitness for a particular purpose, title
+        and non-infringement.
+      </p>
+      <p>
+        {" "}
+        Harmony Hub makes no warranty that (i) the services will meet your
+        requirements or expectations, (ii) that your access to or use of the
+        website and services will be uninterrupted, timely, secure or error
+        free, (iii) that any defects in the website and /or services will be
+        corrected, or (iv) that the website and services or any server through
+        which you access the services are free from viruses or other harmful
+        components.
+      </p>
+      <h2>LIMITATION OF LIABILITY</h2>
+      <p>
+        {" "}
+        Harmony Hub, its affiliates (if any), its licensors, vendors, and their
+        respective directors, officers, employees, and agents shall in no event
+        be responsible or liable to you or to any third-party, whether in
+        contract, warranty, tort (including negligence) or otherwise, for any
+        indirect, special, incidental, consequential, exemplary, liquidated or
+        punitive damages, including but not limited to loss of profit, revenue
+        or business, arising in whole or in part from your access to the website
+        and/or from your use of the services, even if Harmony Hub has been
+        advised of the possibility of such damages.
+      </p>
+      <p>
+        {" "}
+        notwithstanding anything to the contrary in these terms, Harmony Hub’s
+        (including its affiliates (if any), its licensors, vendors and their
+        respective directors, officers, employees, and agents) liability to you
+        for any cause whatsoever and regardless of the form of action, shall be
+        limited to the total amount you paid for the services in the preceding
+        twelve (12) months.
+      </p>
+      <h2> SUSPENSION, TERMINATION AND CANCELLATION OF SERVICES</h2>
+      <p>
+        Harmony Hub may suspend or temporarily disable access to all or part of
+        the Website, User Account, or Services if (i) Harmony Hub suspects you
+        of partaking in any illegal activity; (ii) Harmony Hub reasonably
+        believes that you have violated this Agreement; or (iii) applicable law
+        enforcement or other government agencies have requested Harmony Hub to
+        suspend or temporarily disable your access to the Website and/or
+        Services.
+      </p>
+      <h2> GOVERNING LAW AND JURISDICTION</h2>
+      <p>
+        This Agreement, and any dispute or claim arising out of or in connection
+        with it or its subject matter or formation (including non-contractual
+        disputes or claims), shall be governed by, and construed in accordance
+        with the laws of the United States of America and the Parties
+        irrevocably submit to its exclusive jurisdiction.
+      </p>
+      <h2> GENERAL PROVISIONS</h2>
+      <p>
+        {" "}
+        Severability. If any of the provisions or portions of this Agreement are
+        held to be invalid under any applicable statute or rule of law, they are
+        to that extent deemed to be omitted from this Agreement without in any
+        way invalidating or impairing the other provisions of this Agreement.
+      </p>
+      <p>
+        {" "}
+        No Waiver. A Party’s failure or delay in enforcing any provision of this
+        Agreement will not operate as a waiver of the right to enforce that
+        provision or any other provision of this Agreement at any time. A waiver
+        of any provision of this Agreement shall be in writing, specify the
+        provision to be waived and signed/executed by the party agreeing to the
+        waiver.
+      </p>
+      <p>
+        {" "}
+        No Assignment. You shall not assign or otherwise transfer your rights
+        under this Agreement, without the prior written consent of Harmony Hub.
+        However, this Agreement shall be binding upon Harmony Hub, its
+        affiliates, or any corporation or other entity to which Harmony Hub may
+        (i) transfer all or substantially all its assets and business, and (ii)
+        assign this Agreement, in which case references to “Harmony Hub" as used
+        herein shall mean such affiliate, corporation or other entity.
+      </p>
+      <p>
+        {" "}
+        No Agency, Partnership, Employment or Independent Contractor Status. You
+        acknowledge that Harmony Hub is merely a service provider. Accordingly,
+        there is no employment, agency, joint venture, partnership, or
+        independent contractor-client relationship between you and Harmony Hub.
+      </p>
+      <p>
+        {" "}
+        Notices. Except as otherwise provided under this Agreement, any notice
+        required or permitted to be given will be effective only if it is in
+        writing and sent by certified mail, registered mail, courier or to
+        info@SoundSculpt.app.
+      </p>
+      <p>
+        {" "}
+        Entire Agreement. This Agreement represents the entire agreement between
+        the Parties relating to the subject matter hereof. These Terms alone
+        fully and completely express the agreement of the Parties relating to
+        the subject matter hereof. There are no other courses of dealing,
+        understanding, agreements, representations or warranties, written or
+        oral, except as set forth herein.
+      </p>
+      <h2> CONTACT INFORMATION</h2>
+      <p>
+        Should you have any questions concerning this Agreement and/or any
+        issues or concerns about the Website and/or Services you may contact
+        Harmony Hub at info@SoundSculpt.app.
+      </p>
+    </>
+  );
+}
+export function PrivacyPolicy() {
+  return (
+    <>
+      <h2>Cookies</h2>
+      <p>
+        {" "}
+        Cookies are small files saved on your computer to store data about your
+        connections. This site uses cookies to facilitate the order process and
+        display language and currency.
+      </p>
+
+      <p>
+        The site also uses Google Analytics analysis tool that collects
+        information about your browsing with cookies. These data are collected
+        for the purposes of statistical analysis and are confidential and
+        anonymous.
+      </p>
+
+      <h2>Disable Cookies</h2>
+      <p>
+        You can disable cookies by changing the preferences in your browser, but
+        this may prevent the normal functioning of the site.
+      </p>
+
+      <p>To block or enable cookies</p>
+      <ul>
+        <li>- Internet Explorer</li>
+        <li>- Firefox</li>
+        <li>- Chrome</li>
+        <li>- Safari</li>
+      </ul>
+
+      <h2>Data Gathered When Creating An Account And Order</h2>
+      <p>
+        When you create an account on this site, we store the information needed
+        for orders in a client file. These data are confidential and will not be
+        disclosed to third parties. You can at any time request to view, edit or
+        delete your client informations by sending us your request by mail or
+        email with a copy of your ID.
+      </p>
+
+      <h2>3rd Party Authentication</h2>
+      <p>
+        SoundSculpt use of information received from Google APIs will adhere to
+        Google API Services User Data Policy, including the Limited Use
+        requirements.
+      </p>
+
+      <p>
+        SoundSculpt use of information received from Facebook APIs will adhere
+        to Facebook API Services User Data Policy, including the Limited Use
+        requirements.
+      </p>
+    </>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,27 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import { TOS, PrivacyPolicy } from './TOS.tsx';
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element:  <App />,
+  },
+  {
+    path: "/terms-of-service",
+    element:  <TOS />,
+  },
+  {
+    path: "/privacy-policy",
+    element:  <PrivacyPolicy />,
+  },
+]);
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+     <RouterProvider router={router} />
   </React.StrictMode>,
 )
